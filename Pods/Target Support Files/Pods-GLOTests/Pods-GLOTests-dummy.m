df06fb72948a3df1ad436625d016009436656654
Stephen Looney <stephen@shlooney.com>
2016-08-16 03:03:30 -0500

    Removed pods directory from git for time being, it is too large to fit in regular github repo. See previous commit for details of what all was changed.

--- Pods/Target Support Files/Pods-GLOTests/Pods-GLOTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GLOTests : NSObject
-@end
-@implementation PodsDummy_Pods_GLOTests
-@end
--- Pods/Target Support Files/Pods-GLOTests/Pods-GLOTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_GLOTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_GLOTestsVersionString[];
-
